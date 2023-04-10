32969d644f7fad90dd221bd8a52462b4ce0f0650
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-04-10 19:48:11 -0300

    login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,75 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { FaUser } from 'react-icons/fa'
+import './Login.css';
 
 export function Login() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+       
+      })
+    
+      const { email, password } = formData
+
+      const onChange = (e) => {
+        setFormData((prevState) => ({
+          ...prevState,
+         [e.target.name]: e.target.value,
+        }))
+      }
+    
+      const onSubmit = (e) => {
+        e.preventDefault()
+    
+      }      
+    
+
+
   return (
-    <div>Login</div>
+    <>
+    <section className='heading'>
+        <h1>
+          <FaUser /> Login
+        </h1>
+        <p>Iniciar sesion</p>
+    </section>
+
+    <section className='form'>
+        <form onSubmit={onSubmit}>
+          
+          <div className='form-group'>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
+              value={email}
+              placeholder='Enter your email'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              value={password}
+              placeholder='Enter password'
+              onChange={onChange}
+            />
+          </div>
+         
+          <div className='form-group'>
+            <button type='submit' className='btn btn-block'>
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    
+    </>
   )
 }
 
