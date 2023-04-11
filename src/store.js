68c2966dd5369c987f4e5f2b1f247c6182c66f49
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-04-10 21:33:58 -0300

    reducer para auth

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from '../src/reducers/cart/cartSlice'
+import authReducer from '../src/reducers/user/useSlice'
 
 export const store  = configureStore({
     reducer: {
         cart: cartReducer,
+        auth: authReducer,
     },
 })
